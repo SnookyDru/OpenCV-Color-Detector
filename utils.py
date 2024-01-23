d03497a405753d216ca2cb06cd486d11661f260a
Dhruv Kumar <dhruv@github.com>
2024-01-23 09:30:51 -0800

    removed unnecessary line

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-
 def get_limits(color):
     c = np.uint8([[color]]) # here insert BGR value which you wanna convert into HSV
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
