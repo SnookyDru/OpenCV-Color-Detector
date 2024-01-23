e852fd7a1477896f447905302617388e4112c692
Dhruv Kumar <dhruv@github.com>
2024-01-23 09:05:37 -0800

    Working code added

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-import os
+#import os
 import cv2
+from utils import get_limits
+from PIL import Image
 
 cap = cv2.VideoCapture(0)
+red = [0, 0, 255] # color red in BGR colorspace
 
 while True:
     ret, frame = cap.read()
     hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    cv2.inRange(hsvImage)
+
+    lowerLimit, upperLimit = get_limits(red)
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    mask_ = Image.fromarray(mask)
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 0), 5)
 
     cv2.imshow("frame", frame)
-
     if cv2.waitKey(0) & 0xff == ord('q'):
         break
 
